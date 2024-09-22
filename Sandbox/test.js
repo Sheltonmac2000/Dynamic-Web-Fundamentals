5d2fbefca256128b90199bed4c17e7f4ff595f6e
Shelton Macamo <shelton.mac2000@gmail.com>
2024-09-21 22:25:43 -0600

    W01 website

---
+++ Sandbox/test.js
@@ -0,0 +1,9 @@
+console.log("Hello, World!");
+const myApples = 5;
+const friendApples = 3;
+let total = myApples + friendApples;
+console.log("Total:", total);
+
+document.getElementById("myAppleElement").textContent = myApples;
+document.getElementById("friendAppleElement").textContent = friendApples;
+document.getElementById("totalApplesElement").textContent = total;
