77a44c7961fbf9ff7fd1ed2fb69d185c7d802a19
Shelton Macamo <shelton.mac2000@gmail.com>
2024-10-29 22:12:47 -0600

    Completed Assignments

---
+++ sandbox2/index.js
@@ -0,0 +1,8 @@
+document.getElementById("change").innerHTML = "Hello World";
+document.getElementById("addition").innerHTML = 2 + 2;
+
+// Function to display the date
+function showDate() {
+    document.getElementById("date").innerHTML = Date();
+
+}
