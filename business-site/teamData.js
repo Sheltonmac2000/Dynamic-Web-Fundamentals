e736add69a83685757204887f359bda336e85673
Shelton Macamo <shelton.mac2000@gmail.com>
2024-11-30 18:16:15 -0700

    Updates with revisions

--- business-site/teamData.js
+++ business-site/teamData.js
@@ -1,18 +1,18 @@
 // teamData.js
 export const teamMembers = [
   {
-    name: "Alberto Tovela",
-    role: "CEO & Founder",
-    description:
-      "Alberto Tovela, is a proud father of a beautiful daughter. Before forming Tovela Bookkeeping and Client Accounting Services, Tovela worked at PWC as an auditor, Private Tax Solutions as Tax Professional, and Cooper Norman as Client Accounting Services (CAS) Accountant doing Bookkeeping and Fractional CFO services. His passion is providing Bookkeeping, Controller, and CFO services to increase profits for business owners.",
-    image: "images/tovela.jpeg",
-  },
-  {
     name: "Fegens Joseph",
     role: "Junior Partner",
     description:
-      " With a lifelong passion for numbers, I’ve dedicated my career to helping others navigate their financial challenges. Originally from Haiti, I’ve had the privilege of living and working in Chile, Argentina, Brazil, Mexico, and now the United States. These diverse experiences have shaped my strong work ethic, patience, and perseverance—qualities I bring to every business I help. I understand the challenges of starting and growing a business, and my mission is to guide businesses toward success, whether it’s improving finances, streamlining operations, or finding more time for what matters most. My partner and I are committed to supporting you in achieving your goals.",
+      "With a lifelong passion for numbers, Fegens Joseph has dedicated his career to helping others navigate their financial challenges. Originally from Haiti, Joseph has had the privilege of living and working in Chile, Argentina, Brazil, Mexico, and now the United States. These diverse experiences have shaped his strong work ethic, patience, and perseverance—qualities he brings to every business he helps. Joseph understands the challenges of starting and growing a business, and his mission is to guide businesses toward success, whether it’s improving finances, streamlining operations, or finding more time for what matters most. Fegens passion is to help business owners proactively plan to save money on taxes.",
     image: "images/team2.jpeg",
+  },
+  {
+    name: "Alberto Tovela",
+    role: "CEO & Founder",
+    description:
+      "Alberto Tovela, is a proud father of a beautiful daughter. Before forming Tovela Bookkeeping and Client Accounting Services, Tovela worked at PWC as an auditor, Private Tax Solutions as a Tax Professional, and Cooper Norman as Client Accounting Services (CAS) Accountant doing Bookkeeping and Fractional CFO services. His passion is providing Bookkeeping, Controller, and CFO services to increase profits for business owners.",
+    image: "images/tovela.jpeg",
   },
   {
     name: "Rachael Tovela",
