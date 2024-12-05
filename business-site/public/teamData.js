9ca6318134c472c580b23745fcaa7544bcc9e1e5
Shelton Macamo <shelton.mac2000@gmail.com>
2024-12-04 17:06:38 -0700

    newer updates

--- business-site/public/teamData.js
+++ business-site/public/teamData.js
@@ -5,20 +5,20 @@ export const teamMembers = [
     role: "Junior Partner",
     description:
       "With a lifelong passion for numbers, Fegens Joseph has dedicated his career to helping others navigate their financial challenges. Originally from Haiti, Joseph has had the privilege of living and working in Chile, Argentina, Brazil, Mexico, and now the United States. These diverse experiences have shaped his strong work ethic, patience, and perseverance—qualities he brings to every business he helps. Joseph understands the challenges of starting and growing a business, and his mission is to guide businesses toward success, whether it’s improving finances, streamlining operations, or finding more time for what matters most. Fegens passion is to help business owners proactively plan to save money on taxes.",
-    image: "/business-site/public/images/team2.jpeg",
+    image: "images/team2.jpeg",
   },
   {
     name: "Alberto Tovela",
     role: "CEO & Founder",
     description:
       "Alberto Tovela, is a proud father of a beautiful daughter. Before forming Tovela Bookkeeping and Client Accounting Services, Tovela worked at PWC as an auditor, Private Tax Solutions as a Tax Professional, and Cooper Norman as Client Accounting Services (CAS) Accountant doing Bookkeeping and Fractional CFO services. His passion is providing Bookkeeping, Controller, and CFO services to increase profits for business owners.",
-    image: "/business-site/public/images/tovela.jpeg",
+    image: "images/tovela.jpeg",
   },
   {
     name: "Rachael Tovela",
     role: " Administrative Assistant",
     description:
       "Rachael Tovela graduated with a Bachelor’s Degree from Brigham Young University-Idaho in the summer of 2022. She has been with the company since the inception and loves our company values. She is a devoted wife and mother to a baby daughter. She keeps our office functional!",
-    image: "/business-site/public/images/team3.jpeg",
+    image: "images/team3.jpeg",
   },
 ];
