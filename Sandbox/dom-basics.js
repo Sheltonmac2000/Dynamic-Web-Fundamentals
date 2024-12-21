2b1c311ada221496bf8410bc7831ffcaf8b78721
Shelton Macamo <shelton.mac2000@gmail.com>
2024-12-21 13:17:23 -0700

    Added Reviews Section

--- Sandbox/dom-basics.js
+++
@@ -1,9 +0,0 @@
-const newImg = document.createElement("img");
-newImg.src = "https://picsum.photos/200/300";
-document.body.append(newImg)
-
-//adding an image through the DOM
-const image2 = document.createElement("img");
-image2.src = "https://picsum.photos/200/400";
-document.body.append(image2);
-
--- sandbox2/index.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("change").innerHTML = "Hello World";
-document.getElementById("addition").innerHTML = 2 + 2;
-
-// Function to display the date
-function showDate() {
-    document.getElementById("date").innerHTML = Date();
-
-}
